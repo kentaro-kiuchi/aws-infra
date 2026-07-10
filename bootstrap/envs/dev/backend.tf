3b02e740ea7d345cf38c4c19ecda3acde53447fa
Kentaro Kiuchi <k.kiuchi0215@gmail.com>
2026-07-11 02:24:20 +0900

    chore: enable remote state

--- bootstrap/envs/dev/backend.tf
+++ bootstrap/envs/dev/backend.tf
@@ -1,12 +1,12 @@
 # Enable this block after the first local-state apply creates the bucket.
-#
-# terraform {
-#   backend "s3" {
-#     bucket              = "129898048085-tfstate"
-#     key                 = "aws-infra/bootstrap/terraform.tfstate"
-#     region              = "ap-northeast-1"
-#     use_lockfile        = true
-#     encrypt             = true
-#     allowed_account_ids = ["129898048085"]
-#   }
-# }
+
+terraform {
+  backend "s3" {
+    bucket              = "129898048085-tfstate"
+    key                 = "aws-infra/bootstrap/terraform.tfstate"
+    region              = "ap-northeast-1"
+    use_lockfile        = true
+    encrypt             = true
+    allowed_account_ids = ["129898048085"]
+  }
+}
